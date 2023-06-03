9d4dad5f17cda0db853d57c84093daf412a0c314
Arman Garg <armangarg36@gamil.com>
2023-06-03 17:12:55 +0530

    Upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+
+// import Footer from './components/Footer';
+// import Form from './components/Form';
+import Contactuspage from './components/Contactuspage';
+
+
+
+function App() {
+  return (
+    <>
+      {/* <Form/> */}
+      <Contactuspage/>
+      {/* <Footer/> */}
+      {/* <Coursespage/> */}
+    </>
+  );
+}
+
+export default App;
