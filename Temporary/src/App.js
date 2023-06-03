315efd157805c90adc7d9530df5bf5e4580fe061
Arman Garg <armangarg36@gamil.com>
2023-06-03 17:09:31 +0530

    Uploaded again

---
+++ Temporary/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Courses from './components/Courses';
+import Testimonial from './components/Testimonial';
+// import Courses from './components/Courses';
+// import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <>
+      <Testimonial title = "Our Students speaks for us"/>
+      <Courses/>
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
